f505b140bda04ca5c953fb0e1509136ddfe8e0c8
fertah-m <m@fertah.fr>
2017-06-20 19:24:39 +0200

    Add files via upload

---
+++ ffDB.php
@@ -0,0 +1,59 @@
+<?php
+class ffDB
+{
+    public $user;
+    public $pass;
+    public $db;
+    public $sqlQuery;
+    public $table;
+    protected $bdc;
+
+    function connect()
+    {
+        try {
+            $this->dbc = new PDO('mysql:host=localhost;dbname=' . $this->db, $this->user, $this->pass);
+        } catch (PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    public function runFetch()
+    {
+        $sth = $this->dbc->prepare($this->sqlQuery);
+        $sth->execute();
+        return $sth->fetch(PDO::FETCH_BOTH);
+    }
+
+    public function doMany($cols, $values)
+    {
+        $table = str_ireplace('.csv', '', $this->table);
+        $columns = implode("`,`", $cols);
+        $values = implode(", :", $values);
+
+        $stmt = $this->dbc->prepare("INSERT INTO $table ($columns) VALUES (:$values)");
+
+        $n = 0;
+        foreach ($cols as $key) {
+            $stmt->bindParam(':' . $key, $values[$n]);
+            $n++;
+        }
+
+        $stmt->execute();
+    }
+
+    public function runRequest()
+    {
+        return $this->dbc->exec($this->sqlQuery);
+    }
+
+    /*
+    $tst = new ffDB();
+    $tst->user = 'admin';
+    $tst->pass = '******';
+    $tst->db = 'db_tst';
+    $tst->connect();
+    $tst->sqlQuery = '';
+    */
+}
+?>
